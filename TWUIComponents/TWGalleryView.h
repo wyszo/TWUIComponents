fcde3256f1fa9226a69080278a2ccd821f5646b5
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-05-18 10:59:21 +0200

    Added a comment for TWGalleryView

--- TWUIComponents/TWGalleryView.h
+++ TWUIComponents/TWGalleryView.h
@@ -5,6 +5,10 @@
 #import <UIKit/UIKit.h>
 
 
+/**
+ TODO: Gallery view should not be a scrollView (as using a scrollView here is just an implementation detail). It should be a UIView with scrollView added later programatically.
+ */
+
 @interface TWGalleryView : UIScrollView
 
 @property (nonatomic, strong) NSArray *images;
