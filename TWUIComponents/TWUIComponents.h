973335f067c1f8cc6a1fd6959974a74a793e086d
Tomasz Wyszomirski <thomas@wyszomirski.co.uk>
2015-10-13 22:51:30 +0200

    chore: TWUIComponents.h file cleaned up

--- TWUIComponents/TWUIComponents.h
+++ TWUIComponents/TWUIComponents.h
@@ -1,13 +1,10 @@
 //
-//  TWUIComponents.h
 //  TWUIComponents
-//
-//  Created by Wyszo on 18/05/15.
-//  Copyright (c) 2015 SynappseTomaszWyszomirski. All rights reserved.
 //
 
 #ifndef TWUIComponents_TWUIComponents_h
 #define TWUIComponents_TWUIComponents_h
 
+#include "TWGalleryView.h"
 
 #endif
